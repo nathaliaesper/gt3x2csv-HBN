328f8f49bf556d21c544f383e188b3ddb3029c9c
Nathalia Esper <nathbianchini@gmail.com>
2022-07-15 12:42:10 -0400

    Add files via upload

---
+++ R/gt3x_2_csv.R
@@ -0,0 +1,296 @@
+#' Convert GT3X Files to Raw CSV
+#'
+#' Convert a GT3X file to the raw CSV format provided by ActiLife.
+#'
+#' @param gt3x_files The file or files to convert. You can provide any of a
+#' path to a single file, a path to a directory, or a vector of file paths.
+#' @param outdir A directory where converted CSV files will be saved. If NULL,
+#' the files are saved in the same directory as the original files.
+#' @param progress Display a progress bar. Defaults to TRUE.
+#' @param parallel Use a parallel backend. Defaults to TRUE.
+#' @param cores If `parallel == TRUE`, how many cores are used for processing.
+#' By default this is the smaller of the number of cores available minus 1, or
+#' the number of files to process.
+#' @param logfile create a log file for debugging. Can be one of `FALSE`
+#' (default; do not create a file), `TRUE` (create the log file at the default
+#' location), or a path for where to store the log file.
+#' @param verbose Logical for if additional information should be displayed.
+#' Defaults to `FALSE`
+#' @param recursive If gt3x_files is a directory, should sub-folders be searched
+#' for GT3X  files?
+#' @param actilife The version string for the header. By default, this is
+#' "gt3x2csv v0.2.0". If your analysis depends on knowing an Actilife version,
+#' you can provide one here.
+#'
+#' @importFrom foreach %dopar%
+#'
+#' @return Nothing. Files are saved as a side effect.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' gt3x_2_csv(
+#'   gt3x_files = my_directory,
+#'   outdir = NULL, # Save to the same place
+#'   progress = FALSE, # Show a progress bar?
+#'   parallel = TRUE # Process files in parallel?
+#' )
+#' }
+gt3x_2_csv <- function(gt3x_files,
+                       outdir = NULL,
+                       progress = FALSE,
+                       parallel = TRUE,
+                       cores = NULL,
+                       logfile = FALSE,
+                       verbose = FALSE,
+                       recursive = TRUE,
+                       actilife = FALSE) {
+  start_proc_time <- Sys.time()
+  setup_log(logfile, verbose, outdir)
+
+  proc_type <- check_file_input(gt3x_files)
+
+  # If a directory, convert to a list of files
+  if (proc_type == "directory") {
+    logger::log_trace("Converting directory to a vector of files")
+    gt3x_files <- list_gt3x_rec(gt3x_files, recursive)
+    num_files <- length(gt3x_files)
+    msg <- "Found {crayon::blue(num_files)} GT3X files"
+
+    if (num_files < 1) {
+      stop(msg)
+    }
+    logger::log_info(msg)
+  }
+
+  validate_gt3x_files(gt3x_files, proc_type)
+
+  outfiles <- generate_outputfiles(gt3x_files, outdir)
+
+  # Setup processing backend
+  if (is.null(cores)) {
+    cores <- min(length(gt3x_files), parallel::detectCores() - 1)
+  }
+
+  if (!parallel | proc_type == "single") {
+    logger::log_info("Setting up a sequential backend")
+    foreach::registerDoSEQ()
+  } else {
+    logger::log_info(
+      "Setting up a parallel backend with {crayon::blue(cores)} cores"
+    )
+    cl <- parallel::makeCluster(cores)
+    doSNOW::registerDoSNOW(cl)
+  }
+
+  # Setup progress bar
+  if (progress) {
+    bar <- switch(.Platform$OS.type,
+      windows = {
+        utils::winProgressBar(
+          title = "Converting GT3X files to CSV. Progress:",
+          min = 0,
+          max = length(gt3x_files),
+          label = "0% done",
+          width = 500
+        )
+      },
+      unix = {
+        tcltk::tkProgressBar(
+          title = "Converting GT3X files to CSV. Progress:",
+          min = 0,
+          max = length(gt3x_files),
+          label = "0% done",
+          width = 500
+        )
+      }
+    )
+
+    update_progress <- function(n) {
+      switch(.Platform$OS.type,
+        windows = {
+          utils::setWinProgressBar(bar,
+            n,
+            label = paste0(round(n / length(gt3x_files) * 100, 0), "% done")
+          )
+        },
+        unix = {
+          tcltk::setTkProgressBar(bar,
+            n,
+            label = paste0(round(n / length(gt3x_files) * 100, 0), "% done")
+          )
+        }
+      )
+    }
+
+    opts <- list(progress = update_progress)
+  } else {
+    opts <- list()
+  }
+
+  # Process the files
+  i <- 1
+  foreach::foreach(
+    i = seq_len(length(gt3x_files)),
+    .inorder = FALSE,
+    .export = c("convert_file", "save_header", "save_accel"),
+    .options.snow = opts
+  ) %dopar% {
+    convert_file(gt3x_files[i], outfiles[i])
+  }
+
+  if (parallel & proc_type != "single") parallel::stopCluster(cl)
+
+  if (progress) {
+    close(bar)
+  }
+  comp <- difftime(Sys.time(), start_proc_time)
+  comp_time <- round(as.numeric(comp), 2)
+  comp_units <- attr(comp, "units")
+  msg <- glue::glue(
+    "Finished processing {crayon::blue(length(gt3x_files))} ",
+    "files in {crayon::blue(comp_time, comp_units)}"
+  )
+  logger::log_success(msg)
+}
+utils::globalVariables(c("comp_time", "comp_units"))
+
+#' Convert a Single GT3X File
+#'
+#' Not intended to be called directly. The function to drive the foreach loop.
+#'
+#' @param gt3x_file A path to a single GT3X file
+#' @param outfile The path to save the resulting CSV file
+#' @param actilife The version string for the header. By default, this is
+#' "gt3x2csv v0.2.0". If your analysis depends on knowing an Actilife version,
+#' you can provide one here.
+#'
+#' @return nothing
+convert_file <- function(gt3x_file, outfile, actilife = FALSE) {
+  msg <- glue::glue("Starting conversion.
+                    Input: {crayon::blue(gt3x_file)}
+                    Output: {crayon::blue(outfile)}")
+
+  logger::log_trace(msg)
+  file_start <- Sys.time()
+  gt3x_file_read <- read.gt3x::read.gt3x(gt3x_file, imputeZeroes = TRUE)
+  if (attr(gt3x_file_read, "total_records") == 0) {
+    msg <- glue::glue(
+      "Cannot convert {crayon::blue(gt3x_file)}. ",
+      "File appears to be corrupt or empty"
+    )
+    logger::log_warn(msg)
+  } else {
+    save_header(gt3x_file_read, outfile, actilife)
+    save_accel(gt3x_file_read, outfile)
+
+    comp_time <- round(
+      as.numeric(difftime(Sys.time(),
+        file_start,
+        units = "secs"
+      )),
+      2
+    )
+
+    fsize <- format(structure(file.size(outfile),
+      class = "object_size"
+    ), units = "auto", standard = "SI")
+
+    msg <- glue::glue(
+      "Completed conversion of {crayon::blue(gt3x_file)} in ",
+      "{crayon::blue(comp_time)} seconds. ",
+      "Approx file size: {crayon::blue(fsize)}."
+    )
+    logger::log_success(msg)
+  }
+}
+
+
+#' Save Information to CSV Header
+#'
+#' Reads information from the 'info.txt' file and writes it in the expected
+#' format to a CSV file. Not intended to be called directly.
+#'
+#' @param gt3x_file An object read using `read.gt3x::read.gt3x()`
+#' @param outfile The path to save the resulting CSV file
+#' @param actilife The version string for the header. By default, this is
+#' "gt3x2csv v0.2.0". If your analysis depends on knowing an Actilife version,
+#' you can provide one here.
+#'
+#' @return nothing
+save_header <- function(gt3x_file,
+                        outfile,
+                        actilife = FALSE) {
+  # Header objects ---------------------------------
+  if (!is.character(actilife)) {
+    actilife <- paste("gt3x2csv",
+      getNamespaceVersion("gt3x2csv"),
+      collapse = " "
+    )
+  }
+  logger::log_trace("Getting header information")
+  header <- attr(gt3x_file, "header")
+  start_date_unfmt <- header$`Start Date`
+  dwnld_date_unfmt <- header$`Download Date`
+
+  sample_rate <- attr(gt3x_file, "sample_rate")
+  serial_num <- header$`Serial Number`
+  start_time <- format(start_date_unfmt, format = "%H:%M:%S")
+  start_date <- gsub("^0", "", format(start_date_unfmt, "%d/%m/%Y"))
+  dwnld_time <- format(dwnld_date_unfmt, format = "%H:%M:%S")
+  dwnld_date <- gsub("^0", "", format(dwnld_date_unfmt, "%d/%m/%Y"))
+  batt_volt <- gsub(",", "", header$`Battery Voltage`)
+  firmware <- header$Firmware
+
+  # Header Text -------------------------------------
+  header_txt <-
+    # nolint start
+    glue::glue(
+      "------------ Data File Created By ActiGraph GT3X+ {actilife} Firmware v{firmware} date format d/MM/yyyy at {sample_rate} Hz  Filter Normal -----------\n",
+      "Serial Number: {serial_num}\n",
+      "Start Time {start_time}\n",
+      "Start Date {start_date}\n",
+      "Epoch Period (hh:mm:ss) 00:00:00\n",
+      "Download Time {dwnld_time}\n",
+      "Download Date {dwnld_date}\n",
+      "Current Memory Address: 0\n",
+      "Current Battery Voltage: {batt_volt}     Mode = 12\n",
+      "--------------------------------------------------\n\n"
+    )
+  # nolint end
+  logger::log_trace("Writing header to CSV")
+  cat(header_txt, file = outfile)
+}
+utils::globalVariables(c(
+  "sample_rate", "serial_num", "start_time",
+  "start_date", "dwnld_time", "dwnld_date", "batt_volt",
+  "firmware"
+))
+
+#' Write Activity Data to CSV
+#'
+#' Write the activity data to a CSV file. Not intended to be called directly.
+#'
+#' @param gt3x_file An object read using `read.gt3x::read.gt3x()`
+#' @param outfile The path to save the resulting CSV file
+#'
+#' @return Nothing
+save_accel <- function(gt3x_file, outfile) {
+  outdata <- data.table::as.data.table(gt3x_file)
+  missing <- attr(gt3x_file, "missingness")
+  start_num <- attr(gt3x_file, "start_time_param")
+  sample_rate <- attr(gt3x_file, "sample_rate")
+
+
+  outdata <- impute_missing(outdata, missing, start_num, sample_rate)
+
+  names(outdata) <- c("Accelerometer X", "Accelerometer Y", "Accelerometer Z")
+
+  logger::log_trace("Writing activity to CSV")
+  vroom::vroom_write(outdata,
+    file = outfile,
+    append = TRUE,
+    delim = ",",
+    col_names = TRUE
+  )
+}
---
+++ tests/testthat/test-gt3x_2_csv.R
@@ -0,0 +1,356 @@
+# Turn off the logging
+logger::log_threshold("FATAL")
+
+test_that("save_header() matches a known good output", {
+  dir <- local_dir_with_files(num_files = 1, inc_good = TRUE)
+
+  # Read in the 'known good file'
+  acti_file <- read_proc_csv(file.path(dir, "actilife_file.csv"),
+    header = TRUE,
+    accel_data = FALSE
+  )
+
+  # Process the same file in gt3x2csv
+  actilife_ver <- "ActiLife v6.11.9"
+  gt3x_file <- list_gt3x_rec(dir)
+  read_gt3x_file <- read.gt3x::read.gt3x(gt3x_file, imputeZeroes = TRUE)
+
+  outfile <- file.path(dir, "test_header.csv")
+
+  expect_error(
+    save_header(read_gt3x_file,
+      outfile = outfile,
+      actilife = actilife_ver
+    ),
+    NA
+  )
+  test_header <- read_proc_csv(outfile, header = TRUE, accel_data = FALSE)
+  expect_equal(test_header, acti_file)
+})
+
+test_that("save_accel() matches a known good output", {
+  dir <- local_dir_with_files(num_files = 1, inc_good = TRUE)
+
+  # Read in the 'known good file'
+  acti_file <- read_proc_csv(file.path(dir, "actilife_file.csv"),
+    header = FALSE,
+    accel_data = TRUE
+  )$accel_data
+
+  # Process the same file in gt3x2csv
+  gt3x_file <- list_gt3x_rec(dir)
+  read_gt3x_file <- read.gt3x::read.gt3x(gt3x_file, imputeZeroes = TRUE)
+
+  outfile <- file.path(dir, "test_accel.csv")
+
+  expect_error(
+    save_accel(read_gt3x_file,
+      outfile = outfile
+    ),
+    NA
+  )
+
+  test_accel <- readr::read_csv(outfile, show_col_types = FALSE, lazy = FALSE)
+
+  # Too slow to compare the full data frame
+  expect_equal(dim(test_accel), dim(acti_file))
+  expect_equal(names(test_accel), names(acti_file))
+  expect_identical(sum(test_accel[, 1]), sum(acti_file[, 1]))
+  expect_identical(sum(test_accel[, 2]), sum(acti_file[, 2]))
+  expect_identical(sum(test_accel[, 3]), sum(acti_file[, 3]))
+})
+
+test_that("convert_file() creates correct output", {
+  dir <- local_dir_with_files(num_files = 1, inc_good = TRUE)
+
+  # Read in the 'known good file'
+  acti_file <- read_proc_csv(file.path(dir, "actilife_file.csv"),
+    header = TRUE,
+    accel_data = TRUE
+  )
+
+  # Process the same file in gt3x2csv
+  actilife_ver <- "ActiLife v6.11.9"
+  gt3x_file <- list_gt3x_rec(dir)
+
+  outfile <- file.path(dir, "test_full.csv")
+
+  expect_error(convert_file(gt3x_file, outfile, actilife = actilife_ver), NA)
+
+  test_full <- read_proc_csv(outfile, header = TRUE, accel_data = TRUE)
+
+  # Check the header
+  expect_equal(test_full$header, acti_file$header)
+  # Check the accel data
+  expect_equal(dim(test_full$accel_data), dim(acti_file$accel_data))
+  expect_equal(names(test_full$accel_data), names(acti_file$accel_data))
+  expect_identical(
+    sum(test_full$accel_data[, 1]),
+    sum(acti_file$accel_data[, 1])
+  )
+  expect_identical(
+    sum(test_full$accel_data[, 2]),
+    sum(acti_file$accel_data[, 2])
+  )
+  expect_identical(
+    sum(test_full$accel_data[, 3]),
+    sum(acti_file$accel_data[, 3])
+  )
+})
+
+test_that("gt3x_2_csv() can run as per example", {
+  dir <- local_dir_with_files(num_files = 2)
+
+  expect_error(gt3x_2_csv(dir), NA)
+  expect_equal(length(grep("RAW.csv$", list.files(dir))), 2)
+})
+
+
+# Tests on larger files --------------------------------------------------------
+
+test_that("convert_file() works on 30hz small file", {
+  skip_on_ci()
+  skip_on_cran()
+  skip_if(!file.exists(test_path("large_tests", "small_file.gt3x")))
+
+  dir <- local_dir_with_files(num_files = 0)
+
+  # Read in the 'known good' files
+  acti_file <- read_proc_csv(test_path("large_tests", "small_actilife.csv"),
+    header = TRUE,
+    accel_data = TRUE
+  )
+
+  # Process the same file in gt3x2csv
+  actilife_ver <- "ActiLife v6.11.9"
+  gt3x_file <- test_path("large_tests", "small_file.gt3x")
+
+  outfile <- file.path(dir, "test_small.csv")
+
+  expect_error(
+    convert_file(gt3x_file, outfile, actilife = actilife_ver),
+    NA
+  )
+
+  test_file <- read_proc_csv(outfile, header = TRUE, accel_data = TRUE)
+
+  # Check the header
+  expect_equal(test_file$header, acti_file$header)
+  # Check the accel data
+  expect_equal(
+    dim(test_file$accel_data),
+    dim(acti_file$accel_data)
+  )
+
+  expect_equal(
+    names(test_file$accel_data),
+    names(acti_file$accel_data)
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 1]),
+    sum(acti_file$accel_data[, 1])
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 2]),
+    sum(acti_file$accel_data[, 2])
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 3]),
+    sum(acti_file$accel_data[, 3])
+  )
+})
+
+test_that("convert_file() works on 100hz medium file", {
+  skip_on_ci()
+  skip_on_cran()
+  skip_if(!file.exists(test_path("large_tests", "medium_file.gt3x")))
+
+  dir <- local_dir_with_files(num_files = 0)
+
+  # Read in the 'known good' files
+  acti_file <- read_proc_csv(test_path("large_tests", "medium_actilife.csv"),
+    header = TRUE,
+    accel_data = TRUE
+  )
+
+  # Process the same file in gt3x2csv
+  actilife_ver <- "ActiLife v6.11.9"
+  gt3x_file <- test_path("large_tests", "medium_file.gt3x")
+
+  outfile <- file.path(dir, "test_medium.csv")
+
+  expect_error(
+    convert_file(gt3x_file, outfile, actilife = actilife_ver),
+    NA
+  )
+
+  test_file <- read_proc_csv(outfile, header = TRUE, accel_data = TRUE)
+
+  # Check the header
+  expect_equal(test_file$header, acti_file$header)
+  # Check the accel data
+  expect_equal(
+    dim(test_file$accel_data),
+    dim(acti_file$accel_data)
+  )
+
+  expect_equal(
+    names(test_file$accel_data),
+    names(acti_file$accel_data)
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 1]),
+    sum(acti_file$accel_data[, 1])
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 2]),
+    sum(acti_file$accel_data[, 2])
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 3]),
+    sum(acti_file$accel_data[, 3])
+  )
+})
+
+test_that("convert_file() works on 100hz large file", {
+  skip_on_ci()
+  skip_on_cran()
+  skip_if(!file.exists(test_path("large_tests", "large_file.gt3x")))
+
+  dir <- local_dir_with_files(num_files = 0)
+
+  # Read in the 'known good' files
+  acti_file <- read_proc_csv(test_path("large_tests", "large_actilife.csv"),
+    header = TRUE,
+    accel_data = TRUE
+  )
+
+  # Process the same file in gt3x2csv
+  actilife_ver <- "ActiLife v6.11.9"
+  gt3x_file <- test_path("large_tests", "large_file.gt3x")
+
+  outfile <- file.path(dir, "test_large.csv")
+
+  expect_error(
+    convert_file(gt3x_file, outfile, actilife = actilife_ver),
+    NA
+  )
+
+  test_file <- read_proc_csv(outfile, header = TRUE, accel_data = TRUE)
+
+  # Check the header
+  expect_equal(test_file$header, acti_file$header)
+  # Check the accel data
+  expect_equal(
+    dim(test_file$accel_data),
+    dim(acti_file$accel_data)
+  )
+
+  expect_equal(
+    names(test_file$accel_data),
+    names(acti_file$accel_data)
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 1]),
+    sum(acti_file$accel_data[, 1])
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 2]),
+    sum(acti_file$accel_data[, 2])
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 3]),
+    sum(acti_file$accel_data[, 3])
+  )
+})
+
+test_that("convert_file() works if the last row is missing", {
+  skip_on_ci()
+  skip_on_cran()
+  skip_if(!file.exists(test_path("large_tests", "last_missing.gt3x")))
+
+  dir <- local_dir_with_files(num_files = 0)
+
+  # Read in the 'known good' files
+  acti_file <- read_proc_csv(test_path(
+    "large_tests",
+    "last_missing_actilife.csv"
+  ),
+  header = TRUE,
+  accel_data = TRUE
+  )
+
+  # Process the same file in gt3x2csv
+  actilife_ver <- "ActiLife v6.11.9"
+  gt3x_file <- test_path("large_tests", "last_missing.gt3x")
+
+  outfile <- file.path(dir, "test_last_missing.csv")
+
+  expect_error(
+    convert_file(gt3x_file, outfile, actilife = actilife_ver),
+    NA
+  )
+
+  test_file <- read_proc_csv(outfile, header = TRUE, accel_data = TRUE)
+
+  # Check the header
+  expect_equal(test_file$header, acti_file$header)
+  # Check the accel data
+  expect_equal(
+    dim(test_file$accel_data),
+    dim(acti_file$accel_data)
+  )
+
+  expect_equal(
+    names(test_file$accel_data),
+    names(acti_file$accel_data)
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 1]),
+    sum(acti_file$accel_data[, 1])
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 2]),
+    sum(acti_file$accel_data[, 2])
+  )
+
+  expect_identical(
+    sum(test_file$accel_data[, 3]),
+    sum(acti_file$accel_data[, 3])
+  )
+})
+
+test_that("convert_file() skips corrupt files", {
+  skip_on_ci()
+  skip_on_cran()
+  skip_if(!file.exists(test_path("large_tests", "corrupt_file.gt3x")))
+
+  dir <- local_dir_with_files(num_files = 0)
+
+  gt3x_file <- test_path("large_tests", "corrupt_file.gt3x")
+
+  outfile <- file.path(dir, "test_corrupt.csv")
+
+  logger_thresh <- logger::log_threshold()
+  logger::log_appender(logger::appender_stdout)
+  logger::log_threshold("WARN")
+  expect_output(
+    suppressWarnings(convert_file(gt3x_file, outfile)),
+    "WARN.* Cannot convert.*"
+  )
+  logger::log_threshold(logger_thresh)
+
+  # There should be no output file
+  expect_false(file.exists(outfile))
+})
